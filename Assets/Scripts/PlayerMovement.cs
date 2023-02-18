aa827d124d344254c08594532fa36c3659607330
bilalerdemm <bilalerdemm96@gmail.com>
2023-02-18 11:26:00 +0300

    Playerin hareketi ve animasyonlari saglandi

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,14 +7,20 @@ public class PlayerMovement : MonoBehaviour
     public FloatingJoystick floatingJoystick;
     public GameObject backGround;
     public float speed;
+    public Animator playerAnim;
 
     void Update()
     {
         if (backGround.activeInHierarchy)
         {
-        Vector3 direction = Vector3.forward * floatingJoystick.Vertical + Vector3.right * floatingJoystick.Horizontal;
-        transform.position += direction * speed * Time.deltaTime;
-        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), 10f * Time.deltaTime);
+            Vector3 direction = Vector3.forward * floatingJoystick.Vertical + Vector3.right * floatingJoystick.Horizontal;
+            transform.position += direction * speed * Time.deltaTime;
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), 10f * Time.deltaTime);
+            playerAnim.SetBool("isRunning", true);
+        }
+        else
+        {
+            playerAnim.SetBool("isRunning", false);
         }
     }
 }
