80a77ab0a30645e576a1f9d620a85575ef35984e
bilalerdemm <bilalerdemm96@gmail.com>
2023-02-19 11:31:37 +0300

    Merdivene devam edilecek

--- Assets/Scripts/PlayerGameController.cs
+++ Assets/Scripts/PlayerGameController.cs
@@ -2,14 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
+using Cinemachine;
 
 public class PlayerGameController : MonoBehaviour
 {
     public GameObject spawnPoint;
+    public CinemachineVirtualCamera myCam;
     public Vector3 firstSpawnPointPos;
+    public List<GameObject> collectedStairs;
+    
     private void Awake()
     {
         firstSpawnPointPos = spawnPoint.transform.localPosition;
+    }
+    private void Start()
+    {
+        
     }
     private void Update()
     {
@@ -17,16 +25,24 @@ public class PlayerGameController : MonoBehaviour
         {
             spawnPoint.transform.localPosition = firstSpawnPointPos;
         }
+        if (collectedStairs.Count > 10)
+        {
+            //camera.transform.DOLocalMoveY(25, 1);
+            //camera.transform.DOMove(new Vector3( camera.transform.position.x, 9,camera.transform.position.z), 1f);
+            myCam.transform.DORotate(new Vector3(38, 0, 0), 1f);
+            
+        }
     }
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Blue"))
         {
+            collectedStairs.Add(other.gameObject);
             other.gameObject.transform.parent = transform;
             //other.gameObject.transform.DOMove(spawnPoint.transform.position,1f);
             other.gameObject.transform.DOLocalJump(spawnPoint.transform.localPosition, .5f, 1, 1f);
             other.gameObject.transform.localRotation = spawnPoint.transform.localRotation;
-            spawnPoint.transform.position += new Vector3(0, .15f, 0);
+            spawnPoint.transform.position += new Vector3(0, .13f, 0);
         }
     }
 }
